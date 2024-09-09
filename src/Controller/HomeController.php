f3ba2c1d64f6d94efc76944a84b87e01909d6385
Baylospawn <microsoftbaylo@laposte.net>
2024-09-09 18:23:20 +0200

    Refactor HomeController to render home.html.twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,18 +1,18 @@
 <?php
-
+// src/Controller/HomeController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
+    /**
+     * @Route("/", name="home")
+     */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home.html.twig');
     }
 }
