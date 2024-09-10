344161478bec7d87cfc123902f09e9e2b2fce18d
Baylospawn <microsoftbaylo@laposte.net>
2024-09-10 19:14:12 +0200

    Refactor login functionality to use custom authenticator and remember me feature

--- migrations/Version20240910101420.php
+++ migrations/Version20240910101420.php
@@ -27,7 +27,7 @@ final class Version20240910101420 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE `user`');
-        $this->addSql('DROP TABLE messenger_messages');
+        $this->addSql('DROP TABLE `user`'); 
+        $this->addSql('DROP TABLE messenger_messages'); 
     }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
+    #[Route(path: '/connexion', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
@@ -21,7 +21,10 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', 
+        parameters: 
+        ['last_username' => $lastUsername, 
+        'error' => $error]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
