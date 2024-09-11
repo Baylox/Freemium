f45683f8ea661b7bcf3a86886bdbb307b79ffe43
Baylospawn <microsoftbaylo@laposte.net>
2024-09-11 12:53:14 +0200

    Add commentary to the UserForm file

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email, sorry')] // This is a validation constraint for the email field
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
